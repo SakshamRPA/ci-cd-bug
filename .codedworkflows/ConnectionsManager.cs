3f4f59432f71f3a03b7c241db0903f5999d3d6c1
Saksham Pratap Singh <saksham.singh@yash.com>
2024-11-26 20:10:43 +0530

    1st Commit sps

---
+++ .codedworkflows/ConnectionsManager.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+using System;
+
+namespace BugRestCICDTestCase
+{
+    public class ConnectionsManager
+    {
+        public ConnectionsManager(ICodedWorkflowsServiceContainer resolver)
+        {
+        }
+    }
+}
